93863024d70156d74cf7ea74716b4ddc78a3502c
Saryn Mooney <smooney15@my.whitworth.edu>
2020-07-19 08:50:32 -0600

    added assertion to game_collection test and cleaned up setup

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -5,12 +5,13 @@ class GameCollection
   attr_reader :game_collection
 
   def initialize(game_path)
-    @game_collection = all_games(game_path)
+    @game_path = './data/games_fixture.csv'
+    @game_collection = all_games
   end
 
-  def all_games(game_path)
+  def all_games 
     all_games = []
-    csv = CSV.foreach(game_path, headers: true, converters: :numeric, header_converters: :symbol) do |row|
+    csv = CSV.foreach(@game_path, headers: true, converters: :numeric, header_converters: :symbol) do |row|
       all_games << Game.new(row.to_h)
     end
     all_games
--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -8,12 +8,21 @@ require './lib/game_collection'
 class GameCollectionTest < Minitest::Test
 
   def setup
-    file = './data/games_fixture.csv'
-    @game_collection = GameCollection.new(file)
+    @game_path = './data/games_fixture.csv'
+    @game_collection = GameCollection.new(@game_path)
   end
 
   def test_it_exists
     assert_instance_of GameCollection, @game_collection
+  end
+
+  def test_it_can_return_an_array_of_info
+    @game_collection.all_games
+    assert_instance_of Array, @game_collection.all_games
+
+    assert_equal 20, @game_collection.all_games.count
+
+    assert_equal true, @game_collection.all_games.all? { |game| game.class == Game }
   end
 
 end
