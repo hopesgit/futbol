f555760d6caa81fee17f788a47586df22d50c5b1
Angela Guardia <angelaguardia20@gmail.com>
2020-07-19 11:35:39 -0700

    Refactor initialize method to include a collection instance variable in TeamCollection

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -2,18 +2,17 @@ require 'csv'
 require './lib/team'
 
 class TeamCollection
-  attr_reader :teams
+  attr_reader :all_teams
 
   def initialize(teams_path)
     @teams_csv = teams_path
-    @team_collection = all_teams
+    @all_teams = []
+    create_teams
   end
 
-  def all_teams
-    all_teams = []
+  def create_teams
     CSV.foreach(@teams_csv, headers: true, converters: :numeric, header_converters: :symbol) do |row|
       all_teams << Team.new(row.to_h)
     end
-    all_teams
   end
 end
