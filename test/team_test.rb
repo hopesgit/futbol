a8ac7dd5b5b236f61de788721a16e709f14faa56
Leah Riffell <leah.riffell@gmail.com>
2020-07-18 19:51:17 -0600

    Add test to confirm that team class has readable attributes

--- test/team_test.rb
+++ test/team_test.rb
@@ -4,21 +4,19 @@ require './lib/team'
 
 class TeamTest < MiniTest::Test
   def setup
-    @team = Team.new({id: })
+    @team = Team.new({team_id: 1, franchiseid: 23, teamname: "Atlanta United", abbreviation: "ATL", stadium: "Mercedes-Benz Stadium", link: "/api/v1/teams/1"})
   end
 
   def test_it_exists
     assert_instance_of Team, @team
   end
-end
 
-
-    @merchant = Merchant.new({:id => 5, :name => "Turing School"})
+  def test_it_has_readable_attributes
+    assert_equal 1, @team.id
+    assert_equal 23, @team.franchise_id
+    assert_equal "Atlanta United", @team.name
+    assert_equal "ATL", @team.abbreviation
+    assert_equal "Mercedes-Benz Stadium", @team.stadium
+    assert_equal "/api/v1/teams/1", @team.link
   end
-
-
-@abbreviation="ATL",
- @id=nil,
- @link="/api/v1/teams/1",
- @name="Atlanta United",
- @stadium="Mercedes-Benz Stadium">,
+end
