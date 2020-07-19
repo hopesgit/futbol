c4697ab2d5936819f4b0dacf9248de09eb616ad3
Leah Riffell <leah.riffell@gmail.com>
2020-07-18 19:52:57 -0600

    Make all team attributes readable

--- lib/team.rb
+++ lib/team.rb
@@ -1,5 +1,5 @@
 class Team
-  attr_reader :id, :name, :abbreviation, :stadium, :link
+  attr_reader :id, :franchise_id, :name, :abbreviation, :stadium, :link
 
   def initialize(team_collection_data)
     @id = team_collection_data[:team_id]
