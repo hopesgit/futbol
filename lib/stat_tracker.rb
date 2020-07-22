5dedd65792f37aa133849bc53dbfae8e47cd7491
Angela Guardia <angelaguardia20@gmail.com>
2020-07-21 19:24:16 -0700

    Add total_games_per_team method to StatTracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,7 @@
 require_relative './game'
 require_relative './game_collection'
+require_relative './team_collection'
+require_relative './game_team_collection'
 
 class StatTracker
   attr_reader :game_collection,
@@ -15,12 +17,12 @@ class StatTracker
   end
 
   def initialize(game_path, team_path, game_teams_path)
-    @game_path = game_path
-    @team_path = team_path
-    @game_teams_path = game_teams_path
-    # @team_collection = TeamCollection.new(team_path)
-    @game_collection = GameCollection.new(game_path)
-    @games = @game_collection.all_games
+    game_collection = GameCollection.new(game_path)
+    team_collection = TeamCollection.new(team_path)
+    game_team_collection = GameTeamCollection.new(game_teams_path)
+    @games = game_collection.all_games
+    @teams = team_collection.all_teams
+    @game_teams = game_team_collection.all_game_teams
   end
 
 
@@ -30,6 +32,13 @@ class StatTracker
     @games.reduce({}) do |ids_to_scores, game|
       ids_to_scores[game.game_id] = game.away_goals + game.home_goals
       ids_to_scores
+    end
+  end
+
+  def total_games_per_team
+    @game_teams.reduce(Hash.new(0)) do |result, game_team|
+      result[game_team.team_id] += 1
+      result
     end
   end
 
@@ -47,8 +56,10 @@ class StatTracker
   end
 
   def average_goals_per_game
-    result = total_goals_per_game.values.sum 
+    result = total_goals_per_game.values.sum
     (result / total_goals_per_game.keys.count.to_f).round(2)
   end
+
+# ==================       League Stats Methods      ==================
 
 end
