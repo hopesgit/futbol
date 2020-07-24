e5c6174f1ec0188a2bca68c16e87227bcafcad50
Leah Riffell <leah.riffell@gmail.com>
2020-07-23 21:19:38 -0600

    Fix indent issue

--- lib/game_team_collection.rb
+++ lib/game_team_collection.rb
@@ -13,14 +13,14 @@ class GameTeamCollection
   end
 
   def create_game_teams
-      CSV.foreach(@game_team_path, headers: true, header_converters: :symbol) do |row|
-        game_team = GameTeam.new(row.to_h)
-        @all_game_teams << game_team
-        @all_gameids_per_season.each do |season, game_ids|
-          if game_ids.include?(game_team.game_id)
-            game_team.season = season
-          end
+    CSV.foreach(@game_team_path, headers: true, header_converters: :symbol) do |row|
+      game_team = GameTeam.new(row.to_h)
+      @all_game_teams << game_team
+      @all_gameids_per_season.each do |season, game_ids|
+        if game_ids.include?(game_team.game_id)
+          game_team.season = season
         end
+      end
     end
   end
 end
