6dc2f45fe9e626ef7dbdedd704de48b6200d5081
Saryn Mooney <smooney15@my.whitworth.edu>
2020-07-26 09:12:50 -0600

    cleaned up team helper methods

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -387,9 +387,4 @@ class StatTrackerTest < Minitest::Test
     assert_equal "20172018", @@stat_tracker.worst_season("3")
   end
 
-  def test_it_can_get_favorite_opponent_for_a_team
-    skip
-    assert_equal "FC Dallas", @@stat_tracker.rival("3")
-  end
-
 end
