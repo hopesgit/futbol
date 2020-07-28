e0ee1d69b6aca041ec743f84d4802bbecd835954
Angela Guardia <angelaguardia20@gmail.com>
2020-07-28 15:47:24 -0700

    Add methods for GameTeam creation to DataSet

--- lib/data_set.rb
+++ lib/data_set.rb
@@ -3,8 +3,16 @@ require 'csv'
 class DataSet
   def self.create(path)
     CSV.foreach(path, headers: true, header_converters: :symbol) do |row|
-      all << new(row.to_h)
+      object = new(row.to_h)
+      all << object
+      if object.class == GameTeam
+        object.season = object.generate_season(object.game_id)
+      end
     end
+  end
+
+  def generate_season(game_id)
+    "#{game_id.slice(0, 4)}#{(game_id.slice(0, 4)).to_i + 1}"
   end
 
   def self.count
